59b3459e8808a0bd4d44a99a9990db18be651588
Evan Kiser <kiser.evan1@gmail.com>
2019-10-14 01:48:34 -0500

    added gaussian basics to linsys

--- linsys.py
+++ linsys.py
@@ -27,26 +27,43 @@ class LinearSystem(object):
 
 
     def swap_rows(self, row1, row2):
-        pass # add your code here
-
+        self[row1], self[row2] = self[row2], self[row1]
+        pass
 
     def multiply_coefficient_and_row(self, coefficient, row):
-        pass # add your code here
+        n = self[row].normal_vector
+        k = self[row].constant_term
+        
+        new_normal_vector = n.times_scalar(coefficient)
+        new_constant_term = k * coefficient
 
+        self[row] = Plane(normal_vector=new_normal_vector, constant_term=new_constant_term)
+
+        pass
 
     def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
-        pass # add your code here
+        n1 = self[row_to_add].normal_vector
+        n2 = self[row_to_be_added_to].normal_vector
+        k1 = self[row_to_add].constant_term
+        k2 = self[row_to_be_added_to].constant_term
+
+        new_normal_vector = n1.times_scalar(coefficient).plus(n2)
+        new_constant_term = (k1 * coefficient) + k2
+
+        self[row_to_be_added_to] = Plane(normal_vector=new_normal_vector, constant_term=new_constant_term)
+        
+        pass
 
 
     def indices_of_first_nonzero_terms_in_each_row(self):
         num_equations = len(self)
-        #num_variables = self.dimension
+        num_variables = self.dimension
 
         indices = [-1] * num_equations
 
         for i,p in enumerate(self.planes):
             try:
-                indices[i] = p.first_nonzero_index(p.normal_vector)
+                indices[i] = p.first_nonzero_index(p.normal_vector.coordinates)
             except Exception as e:
                 if str(e) == Plane.NO_NONZERO_ELTS_FOUND_MSG:
                     continue
@@ -85,12 +102,60 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-p0 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+p0 = Plane(normal_vector=Vector([1, 1, 1]), constant_term=1)
+p1 = Plane(normal_vector=Vector([0, 1, 0]), constant_term=2)
+p2 = Plane(normal_vector=Vector([1, 1, -1]), constant_term=3)
+p3 = Plane(normal_vector=Vector([1, 0, -2]), constant_term=2)
 
 s = LinearSystem([p0,p1,p2,p3])
+
+s.swap_rows(0,1)
+if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+        print('Swap test 1 failed')
+
+s.swap_rows(1,3)
+if not (s[0] == p1 and s[1] == p3 and s[2] == p2 and s[3] == p0):
+        print('Swap test 2 failed')
+
+s.swap_rows(3,1)
+if not (s[0] == p1 and s[1] == p0 and s[2] == p2 and s[3] == p3):
+        print('Swap test 3 failed')
+
+s.multiply_coefficient_and_row(-1,2)
+if not (s[0] == p1 and
+        s[1] == p0 and
+        s[2] == Plane(normal_vector=Vector([-1, -1, 1]), constant_term= -3) and
+        s[3] == p3):
+    print('test case 5 failed')
+
+s.multiply_coefficient_and_row(10,1)
+if not (s[0] == p1 and
+        s[1] == Plane(normal_vector=Vector([10, 10, 10]), constant_term= 10) and
+        s[2] == Plane(normal_vector=Vector([-1, -1, 1]), constant_term= -3) and
+        s[3] == p3):
+    print('test case 6 failed')
+
+s.add_multiple_times_row_to_row(0,0,1)
+if not (s[0] == p1 and
+        s[1] == Plane(normal_vector=Vector([10, 10, 10]), constant_term= 10) and
+        s[2] == Plane(normal_vector=Vector([-1, -1, 1]), constant_term= -3) and
+        s[3] == p3):
+    print('test case 7 failed')
+
+s.add_multiple_times_row_to_row(1,0,1)
+if not (s[0] == p1 and
+        s[1] == Plane(normal_vector=Vector([10,11,10]), constant_term= 12) and
+        s[2] == Plane(normal_vector=Vector([-1, -1, 1]), constant_term= -3) and
+        s[3] == p3):
+    print('test case 8 failed')
+
+s.add_multiple_times_row_to_row(-1,1,0)
+if not (s[0] == Plane(normal_vector=Vector([-10, -10, -10]), constant_term= -10) and
+        s[1] == Plane(normal_vector=Vector([10, 11, 10]), constant_term= 12) and
+        s[2] == Plane(normal_vector=Vector([-1, -1, 1]), constant_term= -3) and
+        s[3] == p3):
+    print('test case 9 failed')
+
 
 print(s.indices_of_first_nonzero_terms_in_each_row())
 print('{},{},{},{}'.format(s[0],s[1],s[2],s[3]))
